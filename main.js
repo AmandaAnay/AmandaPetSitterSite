4a79d230e76a89d3af4ee10a8e5a05b57c76ad62
amandaanay <amandinha06@hotmail.com>
2021-07-05 22:20:29 -0300

    v1

---
+++ main.js
@@ -0,0 +1,60 @@
+/* --- Opens and closes the menu when you click on the icons: hamburger and close --- */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* --- When clicking on menu´s items, hide the menu --- */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* --- Show a shadow on page's header when scrolling --- */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    /* --- Scroll is bigger than header's height --- */
+    header.classList.add('scroll')
+  } else {
+    /* --- Scroll is smaller than header's height --- */
+    header.classList.remove('scroll')
+  }
+})
+
+/* --- Testimonials carousel slider Swiper --- */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* --- Scroll Reveal - Show elements when scrolling the page --- */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 500,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
